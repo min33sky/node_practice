4385a8ea957d999af2a285a4f18b57e75191fb8e
min33sky <min33sky@naver.com>
2021-06-14 16:04:36 +0900

    bug fix

--- mini-project-api/routes/v1.js
+++ mini-project-api/routes/v1.js
@@ -1,47 +1,41 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { Domain, User } = require('../models');
+
 const { verifyToken } = require('./middlewares');
+const { Domain, User } = require('../models');
 
 const router = express.Router();
 
-//* JWT 토큰을 클라이언트에게 제공
 router.post('/token', async (req, res) => {
-  const { clientSecret } = req.body; // API 서버에서 발급받은 키
+  const { clientSecret } = req.body;
   try {
     const domain = await Domain.findOne({
-      where: {
-        clientSecret,
-      },
+      where: { clientSecret },
       include: {
-        modle: User,
-        attributes: ['id', 'nick'],
+        model: User,
+        attribute: ['nick', 'id'],
       },
     });
-
     if (!domain) {
       return res.status(401).json({
         code: 401,
         message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
       });
     }
-
     const token = jwt.sign(
       {
-        // JWT payload
         id: domain.User.id,
         nick: domain.User.nick,
       },
-      process.env.JWT_SECRET, // JWT signiture를 만들 비밀키
+      process.env.JWT_SECRET,
       {
-        expiresIn: '1m', //1분
-        issuer: 'min33sky',
+        expiresIn: '1m', // 1분
+        issuer: 'nodebird',
       }
     );
-
     return res.json({
       code: 200,
-      message: '토큰이 발급되었습니다.',
+      message: '토큰이 발급되었습니다',
       token,
     });
   } catch (error) {
@@ -53,7 +47,6 @@ router.post('/token', async (req, res) => {
   }
 });
 
-// JWT 토큰 테스트
 router.get('/test', verifyToken, (req, res) => {
   res.json(req.decoded);
 });
--- mini-project-call/routes/index.js
+++ mini-project-call/routes/index.js
@@ -3,27 +3,26 @@ const axios = require('axios');
 
 const router = express.Router();
 
-//* 토큰 테스트 라우터
 router.get('/test', async (req, res, next) => {
+  // 토큰 테스트 라우터
   try {
-    //? 세션에 토큰이 없으면 토큰 발급 시도
     if (!req.session.jwt) {
+      // 세션에 토큰이 없으면 토큰 발급 시도
       const tokenResult = await axios.post('http://localhost:8002/v1/token', {
         clientSecret: process.env.CLIENT_SECRET,
       });
-      // ? 토큰 발급 성공 & 세션에 토큰 저장
+
       if (tokenResult.data && tokenResult.data.code === 200) {
-        res.session.jwt = tokenResult.data.token;
+        // 토큰 발급 성공
+        req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
       } else {
         // 토큰 발급 실패
-        return res.json(tokenResult.data);
+        return res.json(tokenResult.data); // 발급 실패 사유 응답
       }
     }
     // 발급받은 토큰 테스트
     const result = await axios.get('http://localhost:8002/v1/test', {
-      headers: {
-        authorization: req.session.jwt,
-      },
+      headers: { authorization: req.session.jwt },
     });
     return res.json(result.data);
   } catch (error) {
