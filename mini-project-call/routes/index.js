cea22d8b01bd458db8e86239adff6bb68eb805d0
min33sky <min33sky@naver.com>
2021-06-15 04:47:46 +0900

    Add comments

--- mini-project-call/routes/index.js
+++ mini-project-call/routes/index.js
@@ -6,17 +6,21 @@ const URL = `http://localhost:8002/v2`; // API 서버 주소
 
 axios.defaults.headers.origin = 'http://localhost:4000'; //? origin 헤더 추가
 
+/**
+ * 토큰이 없을 경우 발급받고 API를 요청하는 함수.
+ * 토큰이 만료됐을 때는 재발급 후 API 요청을 한다.
+ */
 const request = async (req, api) => {
   try {
+    //? 세션에 토큰이 없으면 토큰 발급 시도
     if (!req.session.jwt) {
-      //? 세션에 토큰이 없으면 토큰 발급 시도
       const tokenResult = await axios.post(`${URL}/token`, {
-        clientSecret: process.env.CLIENT_SECRET,
+        clientSecret: process.env.CLIENT_SECRET, // API 서버에 도메인 등록시 받은 Key
       });
       req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
     }
 
-    //? API 요청
+    //? API 요청 (요청 헤더에 JWT를 포함해서 요청)
     return await axios.get(`${URL}${api}`, {
       headers: { authorization: req.session.jwt },
     });
