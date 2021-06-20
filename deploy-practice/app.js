b2e7f55b31478c9f349d3935f7f7fceb8f25ddb4
min33sky <min33sky@naver.com>
2021-06-21 03:22:50 +0900

    add: pm2, redis

--- deploy-practice/app.js
+++ deploy-practice/app.js
@@ -6,14 +6,23 @@ const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
 dotenv.config();
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
 
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const passportConfig = require('./passport');
+const logger = require('./logger');
 
 const { sequelize } = require('./models');
 
@@ -35,6 +44,8 @@ sequelize
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
+  app.use(helmet());
+  app.use(hpp());
 } else {
   app.use(morgan('dev'));
 }
@@ -58,6 +69,7 @@ const sessionOption = {
     httpOnly: true,
     secure: false,
   },
+  store: new RedisStore({ client: redisClient }),
 };
 
 if (process.env.NODE_ENV === 'production') {
@@ -81,6 +93,8 @@ app.use('/user', userRouter);
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
+  logger.info('hello');
+  logger.error(error.message);
   next(error);
 });
 
